8e34f00a37e20bc3a40c5baba30d8911138225e3
Arnav Sood <soodarnav01@gmail.com>
2017-07-30 12:01:53 -0400

    Change user type

--- common.jl
+++ common.jl
@@ -7,28 +7,26 @@ include("apikey.jl")
 type TwilioUser
   accountSID::String
   authToken::String
-  toNum::String
   twilNum::String
-  baseURL::String
-  endpoint::String
-  contentType::String
 end
 
-me = TwilioUser("AC4777bd99b1493009cf9f6be876f6ac62", "$authtoken", "+16099778034", "+14159805412", "https://AC4777bd99b1493009cf9f6be876f6ac62:$authtoken@api.twilio.com/2010-04-01", "/Accounts/AC4777bd99b1493009cf9f6be876f6ac62/Messages.json", contentType = "application/x-www-form-urlencoded")
+accountSID = "AC4777bd99b1493009cf9f6be876f6ac62"
+authToken = "$apikey"
+twilNum = "+14159805412"
+baseURL = "https://$accountSID:$authToken@api.twilio.com/2010-04-01"
+endpoint = "/Accounts/$accountSID/Messages.json"
+contentType = "application/x-www-form-urlencoded"
 
-# Function.
-function sendMessage(body, me::TwilioUser)
-  # Unpack params.
+me = TwilioUser(accountSID, authToken, twilNum)
+
+# Function
+function sendMessage(body::String, me::TwilioUser, number::String)
   accountSID = me.accountSID
   authToken = me.authToken
-  toNum = me.toNum
   twilNum = me.twilNum
-  baseURL = me.baseURL
-  endpoint = me.endpoint
-  contentType = me.contentType
 
   # Create request.
-  params = Dict("To" => toNum, "From" => twilNum, "Body" => body, "Content-Type" => contentType)
+  params = Dict("To" => number, "From" => twilNum, "Body" => body, "Content-Type" => contentType)
   resp = post("$baseURL$endpoint"; data = params)
   return JSON.parse(readstring(resp))
 end
